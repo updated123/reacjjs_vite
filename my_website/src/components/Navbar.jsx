f85f31e4c3dacde12ccfdc7044237ad5994be9e1
updated123 <utsavpatel8696@gmail.com>
2024-03-15 23:42:23 +0530

    Nvabar_created

---
+++ my_website/src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Navbar = () => {
+    const Nav=styled.nav`
+    .navbar-list {
+        display: flex;
+        gap: 4.8rem;
+  
+        li {
+          list-style: none;
+  
+          .navbar-link {
+            &:link,
+            &:visited {
+              display: inline-block;
+              text-decoration: none;
+              font-size: 1.8rem;
+              text-transform: uppercase;
+              color: ${({ theme }) => theme.colors.black};
+              transition: color 0.3s linear;
+            }
+  
+            &:hover,
+            &:active {
+              color: ${({ theme }) => theme.colors.helper};
+            }
+          }
+        }
+      }
+    `;
+
+  return (<Nav>
+    <div className='menuIcon'>
+        <ul className='navbar-list'>
+            <li>
+                <NavLink to ="/">Home</NavLink>
+            </li>
+            <li>
+                <NavLink to ="/about">About</NavLink>
+            </li>
+            <li>
+                <NavLink to ="/service">Service</NavLink>
+            </li>
+            <li>
+                <NavLink to ="/contact">Contact</NavLink>
+            </li>
+        </ul>
+    </div>
+  </Nav>
+  );
+  
+
+};
+
+
+
+export default Navbar;
