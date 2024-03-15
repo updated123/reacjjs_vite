0d43368255717794867a647df4e4f8d71eb7da3f
updated123 <utsavpatel8696@gmail.com>
2024-03-15 15:03:39 +0530

    git initilize

---
+++ my_website/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
+import Service from './Services';
+
+const App = () => {
+  return <>
+  <Home/>
+  <About/>
+  <Service/>
+  <Contact/>
+  </>
+}
+
+export default App
