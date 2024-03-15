ba4b6b890b76e48a0a6ae3f5052f3b94ace1223f
updated123 <utsavpatel8696@gmail.com>
2024-03-16 02:46:25 +0530

    Hero

--- my_website/src/components/Header.jsx
+++ my_website/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
-import {Navbar} from './Navbar';
+import  Navbar  from './Navbar';
 import styled from 'styled-components';
 
 const Header = () => {
